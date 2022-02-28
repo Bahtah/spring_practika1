4a582e80397be70dee9098974514accc30133fad
Bahtah <btolikov@gmail.com>
2022-02-28 22:20:35 +0300

    commit

--- src/main/java/peaksoft/model/AnimalsCage.java
+++ src/main/java/peaksoft/model/AnimalsCage.java
@@ -1,13 +1,36 @@
 package peaksoft.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.sql.Time;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
     private Animal animal;
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    @Qualifier("timer")
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    @Autowired
+    @Qualifier("dog")
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
